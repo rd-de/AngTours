0878ba9334e45bebace7a381d1e44990c2ba83b0
elovikov_av <elovikov.01@gmail.com>
2025-04-13 20:49:57 +0500

    create login form

--- src/app/pages/auth/authorization/authorization.component.ts
+++ src/app/pages/auth/authorization/authorization.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { TabsModule } from 'primeng/tabs';
+import { InputTextModule } from 'primeng/inputtext';
+import { Checkbox } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-authorization',
-  imports: [],
+  imports: [NgClass, FormsModule, ButtonModule, TabsModule, InputTextModule, Checkbox],
   templateUrl: './authorization.component.html',
   styleUrl: './authorization.component.scss',
 })
-export class AuthorizationComponent { }
+export class AuthorizationComponent implements OnInit{
+  login: string;
+  password: string;
+  repeatPassword: string;
+  cardNumber: string;
+  email: string;
+  isRemember: boolean;
+  labelText = 'Remember me';
+
+  constructor(){}
+
+  onAuth(): void{}
+  ngOnInit(): void {
+  }
+}
